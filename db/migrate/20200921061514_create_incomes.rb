b5841d0a55699820379308452aa9ad082c2402ea
m-tatsuyama <mt.tatsuyama@gmail.com>
2020-09-26 17:00:27 +0900

    cssを再度設定

--- db/migrate/20200921061514_create_incomes.rb
+++ db/migrate/20200921061514_create_incomes.rb
@@ -1,10 +1,10 @@
 class CreateIncomes < ActiveRecord::Migration[6.0]
   def change
     create_table :incomes do |t|
-      t.string :about_in,           null: false
-      t.integer :in_amount,         null: false
+      t.string :about_in,        null: false
+      t.integer :in_amount,      null: false
       t.references :user,        null: false, foreign_key: true
-      t.date       :month,          null: false
+      t.date       :month,       null: false
 
       t.timestamps
     end
